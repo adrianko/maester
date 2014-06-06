2df7fad3cadd9f5e41b12e2c7f3e8648f52a5041
Adrian Ko <adrianchko@gmail.com>
2014-06-06 22:01:42 +0100

    Add basic views to core app

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from core.models import Board
 
-# Create your views here.
+def home(request):
+    return render_to_response("core/home.html", {"boards": Board.objects.all()})
+
+def board(request):
+    id = request.path.split('/')[2]
+    return HttpResponse("<h1>"+Board.objects.get(pk=id).title+"</h1>")
--- maester/urls.py
+++ maester/urls.py
@@ -9,4 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'core.views.home', name='home'),
+    url(r'^board/[0-9]+$', 'core.views.board', name='boards'),
 )
