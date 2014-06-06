7481604c95cb1faed3695e0b7d43f4228ef33052
Adrian Ko <adrianchko@gmail.com>
2014-06-07 00:22:03 +0100

    ensure int from url

--- core/views.py
+++ core/views.py
@@ -6,6 +6,6 @@ def home(request):
     return render_to_response("core/home.html", {"boards": Board.objects.all()})
 
 def board(request):
-    id = request.path.split('/')[2]
+    id = int(request.path.split('/')[2])
     b = Board.objects.get(pk=id)
     return render_to_response("core/board.html", {"title": b.title, "description": b.description})
