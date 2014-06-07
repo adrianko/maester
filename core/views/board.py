574af1341377fbc76e07db2a0248fa086f259f52
Adrian Ko <adrianchko@gmail.com>
2014-06-07 11:08:10 +0100

    Ensure pk exists before serving

--- core/views/board.py
+++ core/views/board.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http.response import HttpResponseNotFound
 from core.models import Board
 
 def board(request):
     id = int(request.path.split('/')[2])
-    b = Board.objects.get(pk=id)
+    b = get_object_or_404(Board, pk=id)
     return render_to_response("core/board.html", {"title": b.title, "description": b.description})
