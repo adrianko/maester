f9b8f891f1069dbd6c3d37edd784894d46648775
Adrian Ko <adrianchko@gmail.com>
2014-06-07 11:11:32 +0100

    Change object_or_404 to HttpResponseNotFound

--- core/views/board.py
+++ core/views/board.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response
 from django.http.response import HttpResponseNotFound
 from core.models import Board
 
 def board(request):
     id = int(request.path.split('/')[2])
-    b = get_object_or_404(Board, pk=id)
-    return render_to_response("core/board.html", {"title": b.title, "description": b.description})
+    try:
+        b = Board.objects.get(pk=id)
+        return render_to_response("core/board.html", {"title": b.title, "description": b.description})
+    except Board.DoesNotExist:
+        return HttpResponseNotFound("Not Found")
