d45b355f1bbcbe2ddc0fa8e0751e41301c08fbef
Adrian Ko <adrianchko@gmail.com>
2014-06-09 15:24:57 +0100

    Start visuals using bootstrap

--- core/views/board.py
+++ core/views/board.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render_to_response
 from django.http.response import HttpResponseNotFound, Http404
-from api.models import Board
+from api.models import Board, Category
 
 def board(request):
     id = int(request.path.split('/')[2])
     try:
         b = Board.objects.get(pk=id).fields()
         b["page_title"] = "Board: "+b["title"]
+        c = Category.objects.filter(board_id=id)
+        b["categories"] = c
         return render_to_response("core/board.html", b)
     except Board.DoesNotExist:
         raise Http404
