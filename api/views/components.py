bfa050d23f3cb070b7b1cb5254252319694b9d1e
Adrian Ko <adrianchko@gmail.com>
2014-06-12 01:49:40 +0100

    create separate method

--- api/views/components.py
+++ api/views/components.py
@@ -1,6 +1,9 @@
 from django.conf import settings
 
-def task():
-    with file(settings.BASE_DIR+"/core/templates/components/task.html") as f:
+def retrieve(file):
+    with file(settings.BASE_DIR+"/core/templates/components/"+file+".html") as f:
         s = f.read()
     return s
+
+def task():
+    return retrieve("task")
