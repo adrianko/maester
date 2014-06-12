525cc903ad505265fa8a8956075aa8e0c4034b67
Adrian Ko <adrianchko@gmail.com>
2014-06-13 00:25:01 +0100

    add multiple users to new task

--- api/views/tasks.py
+++ api/views/tasks.py
@@ -1,6 +1,7 @@
 from api.models import *
 import components
 from datetime import datetime
+from json import loads
 
 def get(response, id):
     try:
@@ -21,12 +22,23 @@ def create(data):
         response = {"success": False, "request": data}
     else:
         #description, users, duration
-        t = Task.objects.create(
+        t = Task(
             title=data.get("data[title]"),
+            description=data.get("data[desc]"),
             category=Category.objects.get(pk=data.get("data[category]")),
             order=data.get("data[order]"),
+            duration=data.get("data[duration]")+data.get("data[duration_unit]"),
             time_created=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         )
+        t.save()
+        
+        for x in loads(data.get("data[users]")):
+            try:
+                t.users.add(User.objects.get(pk=int(x)))
+                t.save()
+            except User.DoesNotExist:
+                return {"success": False, "request": data}
+
         response = {"success": True, "id": t.fetch()["id"]}
         if data["component"] == "1":
             response["components"] = components.task()
