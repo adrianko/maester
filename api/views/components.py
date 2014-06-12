0d2c7f7d23b4fdad06628a71a81b49bbbf2c5b79
Adrian Ko <adrianchko@gmail.com>
2014-06-12 12:12:57 +0100

    Change var name

--- api/views/components.py
+++ api/views/components.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 
-def retrieve(file):
-    with file(settings.BASE_DIR+"/core/templates/components/"+file+".html") as f:
+def retrieve(filename):
+    with file(settings.BASE_DIR+"/core/templates/components/"+filename+".html") as f:
         s = f.read()
     return s
 
